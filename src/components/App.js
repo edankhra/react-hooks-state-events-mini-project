2cc9b138ce39dfdd487792c9c2a273c2d6ec7273
Ekta <edankhra@gmail.com>
2023-08-17 02:01:58 -0400

    not competed

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, {useState}from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+
+  const onDeleteTask = (taskToBeDeleted) =>{
+    setTasks(tasks.filter((task) => taskToBeDeleted !== task));
+  }
+  
+  const onTaskFormSubmit = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} />
+      <NewTaskForm categories={CATEGORIES.slice(1)} onTaskFormSubmit = {onTaskFormSubmit}/>
+      <TaskList tasks={tasks} onDeleteTask={onDeleteTask}/>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Task from "./Task";
+import {v4 as uuidv4} from 'uuid'
 
-function TaskList() {
+function TaskList({tasks, onDeleteTask}) {
+  
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task) => (<Task onDeleteTask={(task) =>{
+        onDeleteTask(task)}} task={task} key={uuidv4()}/>))}
     </div>
   );
 }
